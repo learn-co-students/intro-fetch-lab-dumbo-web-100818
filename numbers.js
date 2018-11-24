8d7bbfbe6ab3436ab38dd162a8ed8eed42bb6a6e
github username <chao.alexander.123@gmail.com>
2018-11-24 16:13:06 -0500

    complete deliverables to pass the test

--- numbers.js
+++ numbers.js
@@ -1,1 +1,85 @@
 // Write your numbers code in this file!
+function fetchTrivia(num, elementId = "one-facts") {
+  let promiseForData = fetch(`http://numbersapi.com/${num}/trivia`)
+
+  promiseForData
+    .then(response => response.text())
+    .then(function(trivia) {
+      displayTrivia(trivia, elementId)
+    })
+};
+
+
+function displayTrivia(trivia, elementId) {
+  const div = document.getElementById(elementId);
+  div.innerHTML = trivia;
+};
+
+
+function isValidNumber(input) {
+  return isFinite((parseInt(input)))
+};
+
+
+const numberInput = document.getElementById("pick-a-number")
+numberInput.addEventListener("change", function(event) {
+  let input = numberInput.value
+  if (isValidNumber(input)) {
+    fetchTrivia(input, "random-math-fact");
+  } else {
+    const div = document.getElementById("random-math-fact");
+    div.innerHTML = "please enter a valid number";
+  }
+});
+
+function fetchYearFact(year) {
+  let promiseForData = fetch(`http://numbersapi.com/${year}/year`)
+
+  promiseForData
+    .then(response => response.text())
+    .then(showYearFact)
+};
+
+
+function showYearFact(fact) {
+  let div = document.getElementById("year-history");
+  div.innerHTML = fact;
+}
+
+
+function setYearFactInterval() {
+  let year = new Date().getFullYear()
+  fetchYearFact(year)
+  setInterval(() => {
+    year--
+    fetchYearFact(year)
+  }, 5000)
+}
+
+
+function getAllTheNumbers() {
+  let promiseForData = fetch("http://numbersapi.com/1..100");
+
+  return promiseForData.then(response => response.json());
+}
+
+const allNumbersButton = document.querySelector("#all-numbers-button");
+
+allNumbersButton.addEventListener("click", function(event) {
+
+  const div = document.querySelector('#all-the-numbers')
+  div.innerHTML = ''
+  getAllTheNumbers().then(numbers => {
+    let html = '<ul>'
+    for (key in numbers) {
+      html += `<li>${numbers[key]}</li>`
+    }
+    html += '</ul>'
+    div.innerHTML = html
+  })
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  setYearFactInterval()
+})
--- swapi.js
+++ swapi.js
@@ -1,1 +1,72 @@
 // Write your swapi code in this file!
+function getOpeningCrawl() {
+  let promiseForData = fetch('https://swapi.co/api/films/1/')
+  debugger
+  promiseForData
+    .then(response => response.json())
+    .then(displayCrawl)
+}
+
+
+function displayCrawl(film) {
+  const crawlDiv = document.getElementById("crawlDiv");
+  crawlDiv.innerHTML = film.opening_crawl;
+}
+
+
+function displayPlanet(json, elementId) {
+  const planetElement = document.getElementById(elementId);
+
+  planetElement.innerHTML = `
+  <div>Planet Name: ${json.name}</div>
+  <div>Planet Climate: ${json.climate}</div>`
+};
+
+
+function getPlanet(id, elementId = "planetData") {
+  let promiseForData = fetch(`https://swapi.co/api/planets/${id}`)
+
+  promiseForData
+    .then(response => response.json())
+    .then((json) => displayPlanet(json, elementId))
+};
+
+
+const planetForm = document.getElementById("planetForm");
+
+
+planetForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const planetInput = document.getElementById("planetInput");
+  getPlanet(planetInput.value);
+});
+
+
+function getHomePlanet(planetURL) {
+  let elementEventTargetId = event.target.id
+  let id = planetURL.match(/(?<=planets\/)\d/)[0];
+  getPlanet(id, `${elementEventTargetId}-planet`);
+}
+
+
+function displayDroid(droid) {
+  let id = droid.url.match(/(?<=people\/)\d/)[0]
+  let droidDiv = document.getElementById(`droid-${id}`);
+  droidDiv.innerHTML = `
+  <div><b>Name: ${droid.name}</b></div>
+  <div>Height: ${droid.height}</div>
+  <div>Mass: ${droid.mass}</div>
+  <button id = droid-${id}-btn onclick = getHomePlanet("${droid.homeworld}") >Home Planet</button>`
+}
+
+
+function getDroids(id) {
+  let promiseForData = fetch(`https://swapi.co/api/people/${id}`)
+
+  promiseForData
+    .then(response => response.json())
+    .then(displayDroid)
+}
+
+getDroids(3);
+getDroids(4);
